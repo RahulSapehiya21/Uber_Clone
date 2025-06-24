608b909030de04a0f19267ec74dd18782080d7b2
RahulSapehiya <theconfusedrahul1999@gmail.com>
2025-06-24 12:09:00 +0530

    User UI completed

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+
+  // console.log(props);
+  
+  
+  // Sample array for location 
+  const locations = [
+    "24B, Near kapoor's cafe, sheriyans coding school, Bhopal",
+    "22C, Near kapoor's cafe, kendrriya vidayalaya, Bhopal",
+    "20B, Near kapoor's cafe, Army Public school, Bhopal",
+    "18A, Near kapoor's cafe, St. Columbus school, Bhopal",
+
+  ]
   return (
-    <div>LocationSearchPanel</div>
+    <div>
+      
+      {/* this is just a sample data */}
+
+      {
+        locations.map(function(elem,idx){
+          return (
+          <div key={idx} onClick={()=>{
+            props.setvehiclePanel(true)
+            props.setPanelOpen(false)
+          }}
+          className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12"><i className="ri-map-pin-fill"></i></h2>
+        <h4 className="font-medium">{elem}</h4> 
+      </div>
+        );
+        })
+      }
+      
+    </div>
   )
 }
 
-export default LocationSearchPanel      
+export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { useState, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
+import VehiclePanel from "../components/VehiclePanel";
+import ConfirmRide from "../components/ConfirmRide";
+import LookingForDriver from "../components/LookingForDriver";
+import WaitingForDriver from "../components/WaitingForDriver";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
+  const vehiclePanelRef = useRef(null);
+  const confirmRidePanelRef = useRef(null);
   const panelRef = useRef(null);
-  const panelCloseRef = useRef(null)
+  const vehicleFoundRef = useRef(null);
+  const panelCloseRef = useRef(null);
+  const waitingForDriverRef = useRef(null);
+  const [vehiclePanel, setvehiclePanel] = useState(false);
+  const [confirmRidePanel, setConfirmRidePanel] = useState(false);
+  const [vehicleFound, setVehicleFound] = useState(false)
+  const [waitingForDriver, setWaitingForDriver] = useState(false)
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -21,23 +33,85 @@ const Home = () => {
       if (panelOpen) {
         gsap.to(panelRef.current, {
           height: "70%",
-          padding:24
+          padding: 24,
           // opacity:1
-        })
-        gsap.to(panelCloseRef.current,{
-          opacity:1
-        })
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
+        });
       } else {
         gsap.to(panelRef.current, {
           height: "0%",
-          padding:24
+          padding: 24,
           // opacity:0
-        })
-        gsap.to(panelCloseRef.current,{
-          opacity:0
-      })
-    }
-  },[panelOpen]);
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
+        });
+      }
+    },
+    [panelOpen]
+  );
+
+  useGSAP(
+    function () {
+      if (vehiclePanel) {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [vehiclePanel]
+  );
+
+  useGSAP(
+    function () {
+      if (confirmRidePanel) {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [confirmRidePanel]
+  );
+
+  useGSAP(
+    function () {
+      if (vehicleFound) {
+        gsap.to(vehicleFoundRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(vehicleFoundRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [vehicleFound]
+  );
+
+  useGSAP(
+    function () {
+      if (waitingForDriver) {
+        gsap.to(waitingForDriverRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(waitingForDriverRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [waitingForDriver]
+  );
 
   return (
     <div className="h-screen relative overflow-hidden">
@@ -57,13 +131,18 @@ const Home = () => {
 
       <div className=" flex flex-col justify-end h-screen absolute top-0 w-full">
         <div className="h-[30%]  p-6 bg-white relative">
-          <h5 ref={panelCloseRef} onClick={()=>{
-            setPanelOpen(false)
-          }} className="absolute opacity-0 right-6 top-6 text-2xl">
+          <h5
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
+            className="absolute opacity-0 right-6 top-6 text-2xl"
+          >
             <i className="ri-arrow-down-s-line"></i>
           </h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
-          <form className="relative py-3"
+          <form
+            className="relative py-3"
             onSubmit={(e) => {
               submitHandler(e);
             }}
@@ -75,7 +154,7 @@ const Home = () => {
               }}
               value={pickup}
               onChange={(e) => {
-              setPickup(e.target.value);
+                setPickup(e.target.value);
               }}
               className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5"
               type="text"
@@ -94,10 +173,36 @@ const Home = () => {
               placeholder="Add a pick-up location"
             />
           </form>
-          <div ref={panelRef} className="bg-white h-0">
-            <LocationSearchPanel/>
-          </div>
         </div>
+        <div ref={panelRef} className="bg-white h-0">
+          <LocationSearchPanel
+            setPanelOpen={setPanelOpen}
+            setvehiclePanel={setvehiclePanel}
+          />
+        </div>
+      </div>
+      <div
+        ref={vehiclePanelRef}
+        className="fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-10 pt-12"
+      >
+        <VehiclePanel
+          setConfirmRidePanel={setConfirmRidePanel}
+          setvehiclePanel={setvehiclePanel}
+        />
+      </div>
+      <div
+        ref={confirmRidePanelRef}
+        className="fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-6 pt-12"
+      >
+        <ConfirmRide setConfirmRidePanel={setConfirmRidePanel} setVehicleFound={setVehicleFound} setvehiclePanel={setvehiclePanel}/>
+
+        <div ref={vehicleFoundRef} className="fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-6 pt-12">
+          <LookingForDriver setVehicleFound={setVehicleFound}/>
+        </div>
+        <div  ref={waitingForDriverRef}  className="fixed w-full  z-10 bottom-0 bg-white px-3 py-6 pt-12">
+          <WaitingForDriver setWaitingForDriver={setWaitingForDriver}/>
+        </div>
+        
       </div>
     </div>
   );
